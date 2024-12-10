9c363b64399c4f153f8ed48b6d9fd840dbe9ff92
Kamil Ratowski <kamilratowski90@gmail.com>
2024-12-10 11:13:45 +0100

    Created initial logic to fetch asteroids data from external API

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 An example of node proxy server
 
+## Before running the app
+
+Install all dependencies:
+
+```
+npm install
+```
+
+Create `.env` file with all the necessary variables needed for running the app.
+An example of `.env` file:
+
+```
+NASA_API_KEY={your_api_key}
+NEO_FEED_API_URL="https://api.nasa.gov/neo/rest/v1/feed"
+```
+
 ## How to run the app
 
 To run the app use this command:
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+require("dotenv").config();
+
 console.log("Hello world");
+
+const NASA_API_KEY = process.env.NASA_API_KEY;
+
+function formatDateToYearMonthDay(dateToFormat) {
+  return dateToFormat.toISOString().slice(0, 10);
+}
+
+function getAsteroidDates() {
+  const currentDate = new Date();
+  const fiveDaysAgo = new Date();
+
+  // set past date to be 5 days before current date
+  fiveDaysAgo.setDate(currentDate.getDate() - 5);
+
+  return {
+    currentDate: formatDateToYearMonthDay(currentDate),
+    fiveDaysAgo: formatDateToYearMonthDay(fiveDaysAgo),
+  };
+}
+
+async function getAsteroidsData(startDate, endDate) {
+  const NEO_FEED_API_URL = process.env.NEO_FEED_API_URL;
+
+  try {
+    const response = await fetch(
+      `${NEO_FEED_API_URL}?start_date=${startDate}&end_date=${endDate}&api_key=${NASA_API_KEY}`
+    );
+    const parsedResponse = await response.json();
+
+    console.log(parsedResponse.near_earth_objects);
+  } catch (error) {
+    console.error("Error fetching asteroids:", error);
+  }
+}
+
+const { currentDate, fiveDaysAgo } = getAsteroidDates();
+
+getAsteroidsData(currentDate, fiveDaysAgo);
